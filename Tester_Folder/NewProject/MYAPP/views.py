ea3bc448070c54759571e3f36f5a0d38c73bbaa5
SilverliniING <aaryakkw@gmail.com>
2024-03-30 18:24:16 +0530

    url redirection

--- Tester_Folder/NewProject/MYAPP/views.py
+++ Tester_Folder/NewProject/MYAPP/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+#aarya edit
+from django.http import HttpResponse
+
 # Create your views here.
+
+def home(request):
+    return HttpResponse("HomePage")
+def contact(request):
+    return HttpResponse("ContactPage")
+
+
--- Tester_Folder/NewProject/NewProject/urls.py
+++ Tester_Folder/NewProject/NewProject/urls.py
@@ -15,17 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-#aarya edit
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("HomePage")
-def contact(request):
-    return HttpResponse("ContactPage")
-
+from django.urls import path, include 
+#aarya changes
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',home),
-    path('about/',contact),
+    path('',include('MYAPP.urls')),
 ]
