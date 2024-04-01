1400ff6d2eba9a631d42aad662d02e90a854c02a
SilverliniING <aaryakkw@gmail.com>
2024-04-01 14:48:58 +0530

    Cheatsheet_HTML_to_PDF+linkingpages

--- Tester_Folder/NewProject/MYAPP/admin.py
+++ Tester_Folder/NewProject/MYAPP/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# forms.py
+from django import forms
+
+
+
+class MyForm(forms.Form):
+    name = forms.CharField(label='Name', initial='John Doe', widget=forms.Textarea)
+    age = forms.CharField(label='Age', initial=30, widget=forms.Textarea)
--- Tester_Folder/NewProject/MYAPP/urls.py
+++ Tester_Folder/NewProject/MYAPP/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import generate_pdf
 urlpatterns = [
 
     path('',views.home),
     path('contact/',views.contact),
     path('contact2/',views.contact2),
-
+    path('index/', views.index, name='index'),
+    path('generate-pdf/', views.generate_pdf, name='generate_pdf'),
 ]
+
